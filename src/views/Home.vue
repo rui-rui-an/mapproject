03b3a330311243d6c026005b05b5ced06b768f21
rui-rui-an <1405613785@qq.com>
2021-12-12 21:14:26 +0800

    初始化项目

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">home</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {},
+  created () {}
+}
+</script>
+
+<style lang="less" scoped></style>
